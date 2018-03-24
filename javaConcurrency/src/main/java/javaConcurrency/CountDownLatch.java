c87e3a005e74f4d8b7d792e0b7d1e758cdd2dbdb
1367356 <1185978642@qq.com>
2018-03-24 22:31:07 +0800

    commit
    commit

--- javaConcurrency/src/main/java/javaConcurrency/CountDownLatch.java
+++ javaConcurrency/src/main/java/javaConcurrency/CountDownLatch.java
@@ -1,11 +1,27 @@
 package javaConcurrency;
 
+
+import javax.swing.plaf.synth.SynthTabbedPaneUI;
 import java.io.ObjectStreamClass;
 
 public class CountDownLatch {
+    public static void main(String[] args){
+        long timeMillis1 = System.currentTimeMillis();
 
-    public void test1() {
-        java.util.concurrent.CountDownLatch countDownLatch;
-        ObjectStreamClass objectStreamClass;
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        System.out.println(maxMemory);
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        System.out.println(totalMemory);
+
+        test1();
+        long timeMillis2 = System.currentTimeMillis();
+        System.out.println(timeMillis2-timeMillis1);
+    }
+    public static void test1() {
+
+        byte[] b = new byte[10];
+        b[0]=1;
+        b[2]=2;
+
     }
 }
