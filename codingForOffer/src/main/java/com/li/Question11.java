9507674e4307894e621a627610f8dfa89ea0f9af
1367356 <1185978642@qq.com>
2018-04-25 22:36:53 +0800

    commit
    commit

--- codingForOffer/src/main/java/com/li/Question11.java
+++ codingForOffer/src/main/java/com/li/Question11.java
@@ -5,4 +5,59 @@ package com.li;
  */
 public class Question11 {
     
+    boolean invalidInput=false;
+
+    public double power(double base, int exponent) {
+        invalidInput=false;
+        if (equal(base, 0.0) && exponent < 0) {
+            invalidInput=true;
+            return 0.0;
+        }
+        int absExponent=exponent;//求指数的绝对值
+        if (exponent < 0) {
+            absExponent=-exponent;
+        }
+        double result = powerWithUnsignedExponent(base, absExponent);
+        if (exponent < 0) {
+            result=1.0/result;
+        }
+        return result;
+    }
+
+//    /**
+//     * 改进算法，效率更高
+//     * @param base
+//     * @param absExponent
+//     * @return
+//     */
+//    private double powerWithUnsignedExponent(double base, int absExponent) {
+//        if (absExponent == 0) {
+//            return 1;
+//        }
+//        if (absExponent == 1) {
+//            return base;
+//        }
+//        double result = powerWithUnsignedExponent(base, absExponent >> 1);
+//        result=result*result;
+//        if (absExponent & 1 == 1) {
+//            result=result*base;
+//        }
+//        return result;
+//    }
+
+    private double powerWithUnsignedExponent(double base, int absExponent) {
+        double result=1.0;
+        for (int i = 1; i < absExponent; i++) {
+            result=result*base;
+        }
+        return result;
+    }
+
+    private boolean equal(double base, double v) {
+        if (base - v > -0.000000001 && base - v < 0.00000001) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
---
+++ codingForOffer/src/main/java/com/li/Question12.java
@@ -0,0 +1,57 @@
+package com.li;
+
+import org.junit.Test;
+
+/**
+ * 打印1到最大的n位数
+ */
+public class Question12 {
+
+
+    @Test
+    public void test() {
+        print1ToMaxNDigits(2);
+    }
+
+    public void print1ToMaxNDigits(int n) {
+        if (n <= 0) {
+            return;
+        }
+        int[] number = new int[n];
+
+        for (int i = 0; i < 10; i++) {
+            number[0] =  i;
+            print1ToNDigitsRecursively(number, n, 0);
+        }
+    }
+
+    private void print1ToNDigitsRecursively(int[] number, int n, int index) {
+
+        if (index == n - 1) {
+            int head=0;
+            while (number[head] == 0 && head<n-1) {
+                head++;
+            }
+            for (int i = head; i < n; i++) {
+                System.out.print(number[i]);
+            }
+            System.out.println();
+            return;
+        }
+//        if (index == n - 1) {
+//            if(number[0]!=0){
+//            System.out.print(number[0]);
+//            }
+//            for (int i = 1; i < n; i++) {
+//                System.out.print(number[i]);
+//            }
+//            System.out.println();
+//
+//            return;
+//        }
+        for (int i = 0; i < 10; i++) {
+            number[index + 1]=  i;
+            print1ToNDigitsRecursively(number,n,index+1);
+        }
+    }
+}
