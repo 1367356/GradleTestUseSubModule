a999e59887d7a6721c5f2ffc9243fa64d88c41ef
1367356 <1185978642@qq.com>
2018-08-12 08:31:14 +0800

    网易考试

--- niuKe/src/main/java/com/li/wangYi/Question1.java
+++ niuKe/src/main/java/com/li/wangYi/Question1.java
@@ -12,9 +12,6 @@ public class Question1 {
     public static void main(String[] args){
 
         Class clazz = Question1.class.getClass();
-        /**
-         * txt文件中，例如  1 2 4 8 8    1是起始节点，2 相邻节点，4是1,2之间的权重，8是相邻节点，8是1,8之间的权重。
-         */
         InputStream ins = clazz.getResourceAsStream("/wangYi/question1.txt");
         Scanner scanner = new Scanner(ins);
 
--- niuKe/src/main/java/com/li/wangYi/Question2.java
+++ niuKe/src/main/java/com/li/wangYi/Question2.java
@@ -17,42 +17,24 @@ public class Question2 {
         InputStream ins = clazz.getResourceAsStream("/wangYi/question2.txt");
         Scanner scanner = new Scanner(ins);
 
-        String line1 = scanner.nextLine();
-        int n=Integer.parseInt(line1);
-
-        String internet = scanner.nextLine();
-        String[] stringNums = internet.split(" ");
-        int[] interNums = new int[stringNums.length];
-        for (int i = 0; i < stringNums.length; i++) {
-            interNums[i] = Integer.parseInt(stringNums[i]);
+        int n=scanner.nextInt();
+        int[] duiNums = new int[n];   //堆数
+        for (int i = 0; i < n; i++) {
+            duiNums[i]=scanner.nextInt();
         }
+        int askNum = scanner.nextInt();  //询问次数
 
         int[] sumarr = new int[n];
         int sum=0;
         for (int i = 0; i < n; i++) {
-            sum = sum + interNums[i];
+            sum = sum + duiNums[i];
             sumarr[i]=sum;
         }
 
-        String ks=scanner.nextLine();
-        int k = Integer.parseInt(ks);
-        
-        String line3 = scanner.nextLine();
-        String[] stringNum3 = line3.split(" ");
-        int[] intNums3 = new int[k];
-        for (int i = 0; i <k; i++) {
-            intNums3[i] = Integer.parseInt(stringNum3[i]);
-        }
-
-        for (int i = 0; i < k; i++) {
-            int temp = intNums3[i];
-
-
+        for (int i = 0; i < askNum; i++) {
+            int temp = scanner.nextInt();
             //二分查找
             int j=binarySearch(temp, sumarr,0,sumarr.length-1);
-//            while (temp>sumarr[j]){
-//                j++;
-//            }
             System.out.println(j+1);
         }
 
